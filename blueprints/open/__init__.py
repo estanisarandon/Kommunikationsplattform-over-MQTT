d5fb9e9fd65a2c1a265595c0203ac371021985ac
EdvinArnryd98 <edvin.arnryd@student.kyh.se>
2022-02-09 23:00:56 +0100

    Fixed a bug where the signup fields did not need to be filled in

--- blueprints/open/__init__.py
+++ blueprints/open/__init__.py
@@ -1,17 +1,16 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user
-from passlib.hash import argon2
+
 from models import User
+from passlib.hash import argon2
 
-
-
-# Create a blueprint object that can vbe used as an app object for this blueprint
+# Create a blueprint object that can be used as an app object for this blueprint
 bp_open = Blueprint('bp_open', __name__)
 
 
 @bp_open.get('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 @bp_open.get('/login')
@@ -32,13 +31,13 @@ def login_post():
         flash('Wrong email or password')
         return redirect(url_for('bp_open.login_get'))
 
-    # User is veryfied. Login user!
+    # User is verified. Login in user!
     login_user(user)
     user.online = True
 
     from app import db
     db.session.commit()
-    return render_template('index.html')
+    return redirect(url_for('bp_user.user_get'))
 
 
 @bp_open.get('/signup')
@@ -54,13 +53,19 @@ def signup_post():
     hashed_password = argon2.using(rounds=10).hash(password)
 
     # Check if user with this password exists in the database
-    user = User.query.filter_by(email=email).first() # First will five us an object if user exist, or None if not
+    user = User.query.filter_by(email=email).first()  # First will give us an object if user exist, or None if not
+
     if user:
         # If user is not none, then a user with this email exists in the database
         flash("Email address is already in use")
         return redirect(url_for('bp_open.signup_get'))
 
-    new_user = User(name=name, email=email, password=hashed_password, public_key=public_key)
+    if not email:
+        # If the email is empty
+        flash("Please enter an email")
+        return redirect(url_for('bp_open.signup_get'))
+
+    new_user = User(name=name, email=email, password=hashed_password)
 
     from app import db
     db.session.add(new_user)
